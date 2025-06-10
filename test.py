d16848a6f11d6f8cb7c348d037eea4dfe3abdea7
Muhammad Qureshi <taliyah052@gmial.com>
2025-06-09 22:58:48 -0500

    weights and biases added

--- test.py
+++ test.py
@@ -1,23 +1,25 @@
 import numpy as np
 
-output_layer_weights =  np.array([[ 0.09089969, -0.03761097,  0.06690646],
-                                  [ 0.05787504,  0.36574873,  0.15447709],
-                                  [ 0.13360337,  0.10051279,  0.00654227],
-                                  [ 0.10835819,  0.08419912, -0.02765926]])
+derivative_l_over_derivative_b =  np.array([[ -0.3382036,  0.02460542, -0.00745131],
+                                            [ 0.18038728,  0.01465984,  0.17802348],
+                                            [ 0.00177756, -0.02409236, -0.02121836]])
 
 learningrate = 0.01
 
-derivative_l_over_derivative_w =  np.array([[ 0.,         0.,           0.,        ],
-                                            [ 0.00255721, -0.0150004,   0.01734683,],
-                                            [ 0.,          0.,          0.,        ],
-                                            [-0.00076774,  0.00393547, -0.00425434]])
+biases = np.array([1, 2, 3])
 
 
-multiply =  np.dot(learningrate, derivative_l_over_derivative_w)
-print('product: \n', multiply)
+multiply =  np.dot(learningrate, derivative_l_over_derivative_b)
+print("Multiple by 0.01: \n", multiply)
+summing = np.sum(multiply, axis=0, keepdims=True)
+print("Adding: \n", summing)
 
-updateweight = np.subtract(output_layer_weights, multiply)
-print('Updated weight: \n', updateweight)
+newBiases = np.subtract(biases, summing)
+print("NewBiases: \n", newBiases)
+
+# upadtebiases = np.subtract(biases, multiply)
+
+
 
 # -0.0121393571510386 0.0036076339451142 -0.055950551995455
 # -0.0037228112774012  0.0021660471255996  0.1195300502666988
